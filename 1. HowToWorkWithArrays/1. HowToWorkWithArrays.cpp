17c5c37e1fa574900365ed30d5e953fa2055b7d6
Vladimir Novikov <novikovvlal@gmail.com>
2022-04-05 07:49:00 +0300

    Linked list

--- 1. HowToWorkWithArrays/1. HowToWorkWithArrays.cpp
+++ 1. HowToWorkWithArrays/1. HowToWorkWithArrays.cpp
@@ -1,20 +1,48 @@
-// 1. HowToWorkWithArrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float month1 = 100;
+    float month2 = 200;
+    float month3 = 1300;
+    float month4 = 0;
+    float month5 = 200;
+    float month6 = 250;
+
+    float total = month1 + month2 + month3 + month4 + month5 + month6;
+    float average = total / 6;
+    float inTwoYears = average * 24;
+
+    cout << "total: " << total << endl;
+    cout << "average: " << average << endl;
+    cout << "in two years: " << inTwoYears << endl;
+
+    float months[6];// = { month1, month2, month3, month4, month5, month6 };
+    months[0] = month1;
+    months[1] = month2;
+    months[2] = month3;
+    months[3] = month4;
+    months[4] = month5;
+    months[5] = month6;
+
+    total = months[0] + months[1] + months[2] + months[3] + months[4] + months[5];
+
+    cout << "Enter amount: ";
+    cin >> months[0] >> months[1] >> months[2] >> months[3] >> months[4] >> months[5];
+
+    total = 0;
+    for (size_t i = 0; i < sizeof(months) / sizeof(months[0]); i++)
+    {
+        cout << "Enter amount for element " << i++ << ":" << endl;
+        cin >> months[i];
+        total += months[i];
+    }
+
+    int size;
+    cin >> size;
+    //float runtimeSizeNotAllowed[size];
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
